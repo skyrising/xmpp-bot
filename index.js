d5a07874353790b0bf7ae41c9d476467ceb0a325
Simon Wanner <skyrising@pvpctutorials.de>
2017-01-04 23:52:06 +0100

    Respond to disco (XEP-0030) + don't send invalid iq

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const util = require('util')
 const uuid = require('uuid')
 const _ = require('lodash')
 
+const NS_MUC = 'http://jabber.org/protocol/muc'
+const NS_DISCO = 'http://jabber.org/protocol/disco#info'
+const NS_DATA_FORM = 'jabber:x:data'
+
 class XMPPBot {
   constructor (options) {
     options = Object.assign({
@@ -19,7 +23,6 @@ class XMPPBot {
     this.client.on('stanza', this.onStanza.bind(this))
     this.client.on('online', () => {
       debug('online')
-      this.client.send(new XMPP.Stanza('iq'))
       this.client.send(new XMPP.Stanza('presence', {})
         .c('show').t('chat').up()
         .c('status').t('Accepting commands'))
@@ -30,7 +33,7 @@ class XMPPBot {
           debug('Joining ' + room.jid + ' with nick ' + room.nick)
           this.client.send(new XMPP.Stanza('presence', {
             to: room.jid + '/' + room.nick
-          }).c('x', { xmlns: 'http://jabber.org/protocol/muc' }))
+          }).c('x', { xmlns: NS_MUC }))
         })
       }
     })
@@ -86,6 +89,16 @@ class XMPPBot {
         to: stanza.attrs.from
       }))
     }
+    if (stanza.is('iq') && stanza.attrs.type === 'get') {
+      if(stanza.getChild('query', NS_DISCO)) {
+        this.sendIqResult(stanza, new XMPP.Element('query', {xmlns: NS_DISCO})
+          .c('identity', {category: 'client', type: 'bot'}).up()
+          .c('x', {xmlns: NS_DATA_FORM, type: 'result'})
+            .c('field', {var: 'software'}).c('value').t('node-xmpp-client').up().up()
+          .root()
+        )
+      }
+    }
     if (!stanza.is('message') ||
       (stanza.attrs.type !== 'chat' && stanza.attrs.type !== 'groupchat') ||
       stanza.getChild('delay')) return
@@ -114,6 +127,19 @@ class XMPPBot {
       text.slice(1),
       stanza.attrs.type
     )
+  }
+
+  sendIqResult (id, to, result) {
+    if (id instanceof XMPP.Element) {
+      result = to
+      to = id.attrs.from
+      id = id.attrs.id
+    }
+    let iq = new XMPP.Stanza('iq', {id, to, type: 'result'})
+    if (!result) result = []
+    if (result.length === undefined) result = [result]
+    iq.children = result
+    this.client.send(id)
   }
 
   disconnect () {
